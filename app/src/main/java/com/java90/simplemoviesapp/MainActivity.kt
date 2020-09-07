301d51152b26d2c42b47abe649a47eacf04b70e9
javalenciab90 <javalenciab90@gmail.com>
2020-09-06 21:39:48 -0500

    init

---
+++ app/src/main/java/com/java90/simplemoviesapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.java90.simplemoviesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
